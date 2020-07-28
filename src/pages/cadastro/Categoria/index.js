730784008fdda5800e55124b22eda205dabaf6e7
FabianoRF <freis884@gmail.com>
2020-07-28 14:13:03 -0300

    Readme e logo alterados

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,7 +6,16 @@ function CadastroCategoria(){
     return (
         <PageDefault>
             <h1>Cadastro de categoria</h1>
-            
+            <form>
+                <label>
+                    Nome da Categoria
+                    <input type="text"id="nome"/>
+                </label>
+                <button>Cadastrar</button>
+            </form>
+
+
+
             <Link to="/">
                 ir para o home
             </Link>
